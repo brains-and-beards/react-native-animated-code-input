e378ab5ceab7e887628052d310fb51388c4eb1db
Natalia Majkowska <natalia@brainsandbeards.com>
2020-05-27 15:01:04 +0200

    Add fixes for building app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useState, useCallback } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-// import AnimatedNumberInput from 'animated-number-input';
-import AnimatedNumberInput from '../lib/AnimatedNumberInput';
+import AnimatedNumberInput from 'animated-number-input';
 
 const App: FC = () => {
   const [code, setCode] = useState<string>('');
