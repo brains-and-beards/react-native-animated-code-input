a1aad225855bc98da2ffb831fed707d2a3b01baa
Patryk Peszko <me@patrykpeszko.com>
2020-06-01 10:10:56 +0200

    update demo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -47,59 +47,66 @@ const App: FC = () => {
   return (
     <>
       <View style={styles.container}>
-        <Text style={styles.title}>Demo</Text>
+        <Text style={styles.title}>react-native-animated-code-input demo</Text>
 
-        <AnimatedNumberInput
-          code={code}
-          numberOfInputs={NUMBER_OF_INPUTS}
-          onChangeText={onChangeText}
-          onSubmitCode={onSubmit}
-          textColor={"black"}
-          activeCodeContainerStyle={{
-            customStyle: styles.simplyActiveCodeContainer,
-          }}
-          codeContainerStyle={{ customStyle: styles.simplyCustomCodeContainer }}
-          cursorStyle={styles.simplyCursorStyle}
-        />
+        <View>
+          <AnimatedNumberInput
+            code={code}
+            numberOfInputs={NUMBER_OF_INPUTS}
+            onChangeText={onChangeText}
+            onSubmitCode={onSubmit}
+            textColor={"black"}
+            activeCodeContainerStyle={{
+              customStyle: styles.simplyActiveCodeContainer,
+            }}
+            codeContainerStyle={{
+              customStyle: styles.simplyCustomCodeContainer,
+            }}
+            cursorStyle={styles.simplyCursorStyle}
+          />
 
-        <View style={{ margin: 10 }} />
+          <View style={{ margin: 10 }} />
 
-        <AnimatedNumberInput
-          code={code}
-          numberOfInputs={NUMBER_OF_INPUTS}
-          onChangeText={onChangeText}
-          onSubmitCode={onSubmit}
-        />
+          <AnimatedNumberInput
+            code={code}
+            numberOfInputs={NUMBER_OF_INPUTS}
+            onChangeText={onChangeText}
+            onSubmitCode={onSubmit}
+          />
 
-        <View style={{ margin: 10 }} />
+          <View style={{ margin: 10 }} />
 
-        <AnimatedNumberInput
-          code={code}
-          numberOfInputs={NUMBER_OF_INPUTS}
-          onChangeText={onChangeText}
-          onSubmitCode={onSubmit}
-          textColor={"white"}
-          activeCodeContainerStyle={{
-            customStyle: styles.customActiveCodeContainer,
-          }}
-          codeContainerStyle={{ customStyle: styles.customCodeContainer }}
-          cursorStyle={styles.customCursorStyle}
-        />
+          <AnimatedNumberInput
+            code={code}
+            numberOfInputs={NUMBER_OF_INPUTS}
+            onChangeText={onChangeText}
+            onSubmitCode={onSubmit}
+            textColor={"white"}
+            activeCodeContainerStyle={{
+              customStyle: styles.customActiveCodeContainer,
+            }}
+            codeContainerStyle={{ customStyle: styles.customCodeContainer }}
+            cursorStyle={styles.customCursorStyle}
+          />
 
-        <View style={{ margin: 10 }} />
+          <View style={{ margin: 10 }} />
 
-        <AnimatedNumberInput
-          code={code}
-          numberOfInputs={NUMBER_OF_INPUTS}
-          onChangeText={onChangeText}
-          onSubmitCode={onSubmit}
-          textColor={"black"}
-          activeCodeContainerStyle={{
-            customStyle: styles.borderActiveCodeContainer,
-          }}
-          codeContainerStyle={{ customStyle: styles.borderCodeContainer }}
-          cursorStyle={styles.cursorStyle}
-        />
+          <AnimatedNumberInput
+            code={code}
+            numberOfInputs={NUMBER_OF_INPUTS}
+            onChangeText={onChangeText}
+            onSubmitCode={onSubmit}
+            textColor={"black"}
+            activeCodeContainerStyle={{
+              customStyle: styles.borderActiveCodeContainer,
+            }}
+            codeContainerStyle={{ customStyle: styles.borderCodeContainer }}
+            cursorStyle={styles.cursorStyle}
+          />
+          <View style={{ margin: 10 }} />
+        </View>
+
+        <Text style={styles.footnote}>Coded with ❤️ by Brains & Beards</Text>
       </View>
     </>
   );
@@ -108,51 +115,51 @@ const App: FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    paddingTop: Platform.select({
-      android: 0,
-      ios: 50,
-    }),
-    backgroundColor: "white",
+    justifyContent: "space-evenly",
+    backgroundColor: "#ffde1a",
   },
   simplyCustomCodeContainer: {
     backgroundColor: "#ffde1a",
     color: "#c1cefa",
-    borderRadius: 0,
+    borderRadius: 5,
     borderColor: "black",
+    borderWidth: 2,
   },
   simplyCursorStyle: {
-    color: "white",
+    color: "black",
   },
   simplyActiveCodeContainer: {
     backgroundColor: "#ffde1a",
     borderColor: "black",
-    borderRadius: 0,
+    borderRadius: 5,
+    borderWidth: 2,
   },
   customCodeContainer: {
     backgroundColor: "#060054",
     color: "#c1cefa",
-    borderRadius: 0,
+    borderRadius: 5,
   },
   customActiveCodeContainer: {
     backgroundColor: "#c1cefa",
     borderColor: "#060054",
-    borderRadius: 0,
+    borderRadius: 10,
   },
   customCursorStyle: {
     color: "#00b5f5",
   },
   borderActiveCodeContainer: {
-    backgroundColor: "white",
+    backgroundColor: "#ffde1a",
     borderRadius: 50,
     borderColor: "#b2b",
+    borderWidth: 2,
     width: 70,
     height: 70,
   },
   borderCodeContainer: {
-    backgroundColor: "white",
+    backgroundColor: "#ffde1a",
     borderRadius: 50,
     borderColor: "#060054",
+    borderWidth: 2,
     width: 70,
     height: 70,
   },
@@ -163,10 +170,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 35,
     textAlign: "center",
-    marginBottom: Platform.select({
-      android: 20,
-      ios: 50,
-    }),
+  },
+  footnote: {
+    fontSize: 15,
+    textAlign: "center",
   },
 });
 
