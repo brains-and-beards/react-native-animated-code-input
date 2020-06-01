578f1fba003f2db78add4b5e62e8330c6a21b66f
Natalia Majkowska <natalia@brainsandbeards.com>
2020-06-01 13:45:05 +0200

    Add fixes for a layout

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,13 @@
 import React, { FC, useState, useCallback, useEffect } from "react";
-import { Text, View, StyleSheet, Alert, Platform } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Alert,
+  Platform,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
 import AnimatedNumberInput from "animated-number-input";
 import Clipboard from "@react-native-community/clipboard";
 
@@ -45,78 +53,90 @@ const App: FC = () => {
   }, []); // if you want to observe code all the time, replace [] with [code]
 
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.title}>react-native-animated-code-input demo</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <View style={styles.scrollContainer}>
+          <Text style={styles.title}>
+            react-native-animated-code-input demo
+          </Text>
 
-        <View>
-          <AnimatedNumberInput
-            code={code}
-            numberOfInputs={NUMBER_OF_INPUTS}
-            onChangeText={onChangeText}
-            onSubmitCode={onSubmit}
-            textColor={"black"}
-            activeCodeContainerStyle={{
-              customStyle: styles.simplyActiveCodeContainer,
-            }}
-            codeContainerStyle={{
-              customStyle: styles.simplyCustomCodeContainer,
-            }}
-            cursorStyle={styles.simplyCursorStyle}
-          />
+          <View>
+            <AnimatedNumberInput
+              code={code}
+              numberOfInputs={NUMBER_OF_INPUTS}
+              onChangeText={onChangeText}
+              onSubmitCode={onSubmit}
+              textColor={"black"}
+              activeCodeContainerStyle={{
+                customStyle: styles.simplyActiveCodeContainer,
+              }}
+              codeContainerStyle={{
+                customStyle: styles.simplyCustomCodeContainer,
+              }}
+              cursorStyle={styles.simplyCursorStyle}
+            />
 
-          <View style={{ margin: 10 }} />
+            <View style={{ margin: 10 }} />
 
-          <AnimatedNumberInput
-            code={code}
-            numberOfInputs={NUMBER_OF_INPUTS}
-            onChangeText={onChangeText}
-            onSubmitCode={onSubmit}
-          />
+            <AnimatedNumberInput
+              code={code}
+              numberOfInputs={NUMBER_OF_INPUTS}
+              onChangeText={onChangeText}
+              onSubmitCode={onSubmit}
+            />
 
-          <View style={{ margin: 10 }} />
+            <View style={{ margin: 10 }} />
 
-          <AnimatedNumberInput
-            code={code}
-            numberOfInputs={NUMBER_OF_INPUTS}
-            onChangeText={onChangeText}
-            onSubmitCode={onSubmit}
-            textColor={"white"}
-            activeCodeContainerStyle={{
-              customStyle: styles.customActiveCodeContainer,
-            }}
-            codeContainerStyle={{ customStyle: styles.customCodeContainer }}
-            cursorStyle={styles.customCursorStyle}
-          />
+            <AnimatedNumberInput
+              code={code}
+              numberOfInputs={NUMBER_OF_INPUTS}
+              onChangeText={onChangeText}
+              onSubmitCode={onSubmit}
+              textColor={"white"}
+              activeCodeContainerStyle={{
+                customStyle: styles.customActiveCodeContainer,
+              }}
+              codeContainerStyle={{ customStyle: styles.customCodeContainer }}
+              cursorStyle={styles.customCursorStyle}
+            />
 
-          <View style={{ margin: 10 }} />
+            <View style={{ margin: 10 }} />
 
-          <AnimatedNumberInput
-            code={code}
-            numberOfInputs={NUMBER_OF_INPUTS}
-            onChangeText={onChangeText}
-            onSubmitCode={onSubmit}
-            textColor={"black"}
-            activeCodeContainerStyle={{
-              customStyle: styles.borderActiveCodeContainer,
-            }}
-            codeContainerStyle={{ customStyle: styles.borderCodeContainer }}
-            cursorStyle={styles.cursorStyle}
-          />
-          <View style={{ margin: 10 }} />
+            <AnimatedNumberInput
+              code={code}
+              numberOfInputs={NUMBER_OF_INPUTS}
+              onChangeText={onChangeText}
+              onSubmitCode={onSubmit}
+              textColor={"black"}
+              activeCodeContainerStyle={{
+                customStyle: styles.borderActiveCodeContainer,
+              }}
+              codeContainerStyle={{ customStyle: styles.borderCodeContainer }}
+              cursorStyle={styles.cursorStyle}
+            />
+            <View style={{ margin: 10 }} />
+          </View>
         </View>
-
-        <Text style={styles.footnote}>Coded with ❤️ by Brains & Beards</Text>
-      </View>
-    </>
+      </ScrollView>
+      <Text style={styles.footnote}>Coded with ❤️ by Brains & Beards</Text>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-evenly",
     backgroundColor: "#ffde1a",
+  },
+  scrollContainer: {
+    flex: 1,
+    marginTop: 30,
   },
   simplyCustomCodeContainer: {
     backgroundColor: "#ffde1a",
@@ -170,10 +190,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 35,
     textAlign: "center",
+    marginBottom: 60,
   },
   footnote: {
-    fontSize: 15,
     textAlign: "center",
+    marginBottom: 10,
   },
 });
 
